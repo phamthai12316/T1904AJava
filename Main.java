4389be218e79308e5c826f3593cf1b8e52430a68
phamthai12316 <phamthai12316@gmail.com>
2019-10-30 09:22:04 +0700

    LabSession3

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+package labsession3;
 
 public class Main {
     public static void main(String args[]){
+        PhanSo fc = new PhanSo();
+        fc.nhapPhanSo();
+        fc.inPhanSo();
+        fc.rutGonPhanSo();
+        fc.inPhanSo();
+        fc.nghichDaoPhanSo();
+        fc.inPhanSo();
+        PhanSo fc2 = new PhanSo(1,3);
+
+        PhanSo tong = fc.addPhanSo(fc2);
+        char a[] = {'a','b','c','d','e'};
 
     }
 }
